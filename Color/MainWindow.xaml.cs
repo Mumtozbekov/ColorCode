7239667d15aaacf4ff83cc3dcf429905d23b812c
Sarvar Mumtozbekov <mumtozbekov@gmail.com>
2020-10-09 22:07:55 +0300

    Добавьте файлы проекта.

---
+++ Color/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace ColorCode
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MiniWindow mini = new MiniWindow();
+        bool clicked = false;
+        static System.Drawing.Color GetPixel(System.Drawing.Point position)
+        {
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(position, new System.Drawing.Point(0, 0), new System.Drawing.Size(1, 1));
+                }
+                return bitmap.GetPixel(0, 0);
+            }
+        }
+
+        public MainWindow()
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+            InitializeComponent();
+
+            _ = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var colr = GetPixel(GetMousePosition());
+                    mini.tbHex.Text = $"{ colr.A}, { colr.R}, { colr.G}, { colr.B}";
+                    await Task.Delay(5);
+                }
+            });
+            mini.Show();
+            mini.Topmost = true;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static System.Drawing.Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new System.Drawing.Point(w32Mouse.X, w32Mouse.Y);
+        }
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!clicked)
+            {
+                var colr = GetPixel(GetMousePosition());
+                mini.tbRgb.Text = $"{ colr.A}, { colr.R}, { colr.G}, { colr.B}";
+                rec.BorderBrush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(colr.R, colr.G, colr.B));
+                mini.tbHex.Text = rec.BorderBrush.ToString();
+            }
+            else
+            {
+                mini.Focus();
+            }
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            clicked = (e.RightButton == MouseButtonState.Pressed);
+             
+        }
+
+        private void Grid_GotFocus(object sender, RoutedEventArgs e)
+        {
+            clicked = false;
+        }
+    }
+}
---
+++ Color/MiniWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ColorCode
+{
+    /// <summary>
+    /// Логика взаимодействия для MiniWindow.xaml
+    /// </summary>
+    public partial class MiniWindow : Window
+    {
+        public MiniWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void centerGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
